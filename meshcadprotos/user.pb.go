ed4aa7de36c2b25f04eac16ce302faadb0eb6631
Taylor Reid <taylorsreid@gmail.com>
2026-06-27 11:15:14 -1000

    remove UpdateUser.meshAddress, that should be done via AuthorizeUser

--- meshcadprotos/user.pb.go
+++ meshcadprotos/user.pb.go
@@ -88,7 +88,6 @@ type UpdateUser struct {
 	Password      *string                `protobuf:"bytes,3,opt,name=password,proto3,oneof" json:"password,omitempty"`
 	Name          *string                `protobuf:"bytes,4,opt,name=name,proto3,oneof" json:"name,omitempty"`
 	Status        *string                `protobuf:"bytes,5,opt,name=status,proto3,oneof" json:"status,omitempty"`
-	MeshAddress   *uint32                `protobuf:"varint,6,opt,name=meshAddress,proto3,oneof" json:"meshAddress,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -158,13 +157,6 @@ func (x *UpdateUser) GetStatus() string {
 	return ""
 }
 
-func (x *UpdateUser) GetMeshAddress() uint32 {
-	if x != nil && x.MeshAddress != nil {
-		return *x.MeshAddress
-	}
-	return 0
-}
-
 type AuthorizeUser struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	Email         string                 `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
@@ -227,20 +219,18 @@ const file_user_proto_rawDesc = "" +
 	"CreateUser\x12\x14\n" +
 	"\x05email\x18\x01 \x01(\tR\x05email\x12\x1a\n" +
 	"\bpassword\x18\x02 \x01(\tR\bpassword\x12\x12\n" +
-	"\x04name\x18\x03 \x01(\tR\x04name\"\xf0\x01\n" +
+	"\x04name\x18\x03 \x01(\tR\x04name\"\xb9\x01\n" +
 	"\n" +
 	"UpdateUser\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\rR\x02id\x12\x19\n" +
 	"\x05email\x18\x02 \x01(\tH\x00R\x05email\x88\x01\x01\x12\x1f\n" +
 	"\bpassword\x18\x03 \x01(\tH\x01R\bpassword\x88\x01\x01\x12\x17\n" +
 	"\x04name\x18\x04 \x01(\tH\x02R\x04name\x88\x01\x01\x12\x1b\n" +
-	"\x06status\x18\x05 \x01(\tH\x03R\x06status\x88\x01\x01\x12%\n" +
-	"\vmeshAddress\x18\x06 \x01(\rH\x04R\vmeshAddress\x88\x01\x01B\b\n" +
+	"\x06status\x18\x05 \x01(\tH\x03R\x06status\x88\x01\x01B\b\n" +
 	"\x06_emailB\v\n" +
 	"\t_passwordB\a\n" +
 	"\x05_nameB\t\n" +
-	"\a_statusB\x0e\n" +
-	"\f_meshAddress\"A\n" +
+	"\a_status\"A\n" +
 	"\rAuthorizeUser\x12\x14\n" +
 	"\x05email\x18\x01 \x01(\tR\x05email\x12\x1a\n" +
 	"\bpassword\x18\x02 \x01(\tR\bpasswordB\x12Z\x10../meshcadprotosb\x06proto3"
